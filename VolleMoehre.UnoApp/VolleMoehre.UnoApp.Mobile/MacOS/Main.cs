6c1b28893a67ad5047b4de69a8d6a35b352f36b5
TopperDEL <github_com@tparth.de>
2022-01-03 13:33:55 +0100

    feat: WIP to update to latest Uno-App

---
+++ VolleMoehre.UnoApp/VolleMoehre.UnoApp.Mobile/MacOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace VolleMoehre.UnoApp.macOS
+{
+    internal static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new App();
+            NSApplication.Main(args);
+        }
+    }
+}
+
---
+++ VolleMoehre.UnoApp/VolleMoehre.UnoApp.Wasm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace VolleMoehre.UnoApp.Wasm
+{
+    public class Program
+    {
+        private static VolleMoehre.App.App _app;
+
+        static int Main(string[] args)
+        {
+            Windows.UI.Xaml.Application.Start(_ => _app = new VolleMoehre.App.App());
+
+            return 0;
+        }
+    }
+}
