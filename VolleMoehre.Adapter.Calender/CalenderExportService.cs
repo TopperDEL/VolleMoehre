2ed497820741d1aa3d82d51961a276a8ff2c250f
TopperDEL <github_com@tparth.de>
2022-04-26 13:13:19 +0200

    cleanup

--- VolleMoehre.Adapter.Calender/CalenderExportService.cs
+++ VolleMoehre.Adapter.Calender/CalenderExportService.cs
@@ -24,12 +24,6 @@ namespace VolleMoehre.Adapter.Calender
 
         public string TransferToiCalFeed(Spieler spieler, List<Trainingstermin> trainingsTermine, List<Auftrittstermin> auftrittsTermine, List<Ort> orte)
         {
-            byte[] retBytes;
-
-            //Alarm alarm = new Alarm();
-            //alarm.Action =  AlarmAction.Display;
-            //alarm.Description = "Volle-Möhre Kalender für " + spieler.Name;
-            //alarm.Trigger = new Trigger();
             Calendar iCalender = new Calendar();
             iCalender.Method = "PUBLISH";
 
@@ -58,38 +52,9 @@ namespace VolleMoehre.Adapter.Calender
                         moehreEvent.Description = moehreEvent.Description + ", " + beschreibung;
                     }
                 }
-                //moehreEvent.Location = auftritt.Ort.Bezeichnung + " - Freitext: " + auftritt.FreitextInfoIntern;
                 moehreEvent.Summary = moehreEvent.Description;
-                //moehreEvent.Url = new Uri("http://www.vollemoehre.de/Auftritte/IndexIntern/#" + auftritt.TerminID);
-                //moehreEvent.Alarms.Add(alarm);
                 iCalender.Events.Add(moehreEvent);
             }
-            //foreach (Trainingstermin auftritt in auftritteSpielerVGM)
-            //{
-            //    auftritt.OrtReference.Load();
-            //    DDay.iCal.Event moehreEvent = new DDay.iCal.Event();
-            //    moehreEvent.DTStart = new DDay.iCal.iCalDateTime(auftritt.Datum);
-            //    moehreEvent.Duration = TimeSpan.FromHours(2);
-            //    moehreEvent.Description = "VGM Volle Möhre: " + auftritt.Showtyp;
-            //    moehreEvent.Location = auftritt.Ort.Bezeichnung + " - Freitext: " + auftritt.FreitextInfo;
-            //    moehreEvent.Summary = "Vorgemerkt für Volle Möhre: " + auftritt.Showtyp;
-            //    moehreEvent.Url = new Uri("http://www.vollemoehre.de/Auftritte/" + auftritt.TerminID);
-            //    moehreEvent.Alarms.Add(alarm);
-            //    iCalender.Events.Add(moehreEvent);
-            //}
-            //foreach (Training training in trainingsSpielerVGM)
-            //{
-            //    training.OrtReference.Load();
-            //    DDay.iCal.Event moehreEvent = new DDay.iCal.Event();
-            //    moehreEvent.DTStart = new DDay.iCal.iCalDateTime(training.Datum);
-            //    moehreEvent.Duration = TimeSpan.FromHours(2);
-            //    moehreEvent.Description = "VGM Volle Möhre: " + training.FreitextInfo;
-            //    moehreEvent.Location = training.Ort.Bezeichnung + " - Freitext: " + training.FreitextInfo;
-            //    moehreEvent.Summary = "Vorgemerkt für Volle Möhre: " + training.FreitextInfo;
-            //    moehreEvent.Url = new Uri("http://www.vollemoehre.de/Trainings/" + training.TrainingID);
-            //    moehreEvent.Alarms.Add(alarm);
-            //    iCalender.Events.Add(moehreEvent);
-            //}
             foreach (Trainingstermin training in trainingsTermine.Where(a => a.Teilnehmer.Contains(spieler.Id) ||
                                                                              a.Vorgemerkt.Contains(spieler.Id) ||
                                                                              a.Leiter.Contains(spieler.Id) ||
@@ -105,29 +70,23 @@ namespace VolleMoehre.Adapter.Calender
                 else
                 {
                     moehreEvent.Description = "Volle Möhre: " + training.FreitextInfo;
-                }                
-                //moehreEvent.Location = training.Ort.Bezeichnung + " - Freitext: " + training.FreitextInfo;
+                }
+                if (!string.IsNullOrEmpty(training.OrtId) && orte.Where(o => o.Id == training.OrtId).Count() == 1)
+                {
+                    var beschreibung = orte.Where(o => o.Id == training.OrtId).First().Bezeichnung;
+                    moehreEvent.Location = beschreibung + " - Freitext: " + training.FreitextInfo;
+                    if (!string.IsNullOrEmpty(beschreibung) && !beschreibung.Contains("Freitext"))
+                    {
+                        moehreEvent.Description = moehreEvent.Description + ", " + beschreibung;
+                    }
+                }
                 moehreEvent.Summary = moehreEvent.Description;
-                //moehreEvent.Url = new Uri("http://www.vollemoehre.de/Trainings/" + training.TrainingID);
-                //moehreEvent.Alarms.Add(alarm);
                 iCalender.Events.Add(moehreEvent);
             }
             iCalender.AddProperty("CALSCALE", "GREGORIAN");
 
             CalendarSerializer serializer = new CalendarSerializer();
             return serializer.SerializeToString(iCalender);
-
-            //using (System.IO.MemoryStream mstream = new System.IO.MemoryStream())
-            //{
-            //    CalendarSerializer serializer = new CalendarSerializer();
-            //    serializer.SerializeToString(iCalender);
-            //    System.Text.UTF8Encoding utf8OhneBOM = new System.Text.UTF8Encoding(false);
-            //    serializer.Serialize(iCalender, mstream, utf8OhneBOM);
-            //    mstream.Flush();
-            //    retBytes = mstream.GetBuffer();
-            //}
-
-            //return retBytes;
         }
 
         public byte[] TransferToiCalPublic(Auftrittstermin auftrittsTermin)
