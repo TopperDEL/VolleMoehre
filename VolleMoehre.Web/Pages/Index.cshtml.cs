30ceffcdbf7efc5bb62a9a940cf4d5e839e41323
TopperDEL <github_com@tparth.de>
2025-11-21 11:02:13 +0100

    * „Linie 27: The Get<T> method of RestSharp.RestClient in the current version returns the deserialized data directly, not a response object with a Data property. Assign the result of Get<T> directly to Auftritte and apply the ordering.“ in Datei „VolleMoehre.Web\Pages\Index.cshtml.cs“

--- VolleMoehre.Web/Pages/Index.cshtml.cs
+++ VolleMoehre.Web/Pages/Index.cshtml.cs
@@ -24,8 +24,7 @@ namespace VolleMoehre.Web.Pages
         {
             var client = new RestClient("https://api.vollemoehre.de/api/");
             var request = new RestRequest("public/auftritte", Method.Get);
-            var response = client.Get<List<Contracts.Model.AuftrittsterminPublic>>(request);
-            Auftritte = response.Data.OrderBy(a=>a.Datum).ToList();
+            Auftritte = client.Get<List<Contracts.Model.AuftrittsterminPublic>>(request).OrderBy(a=>a.Datum).ToList();
         }
     }
 }
